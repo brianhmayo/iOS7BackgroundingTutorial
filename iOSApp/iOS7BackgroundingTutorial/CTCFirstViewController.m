1e2ff317d5d7cc056124b88da15767be99294f10
Brian H Mayo <brian.h.mayo@gmail.com>
2013-09-21 10:09:52 -0400

    iniital commit of base structure for the iOS app and the base files for the service

---
+++ iOSApp/iOS7BackgroundingTutorial/CTCFirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  CTCFirstViewController.m
+//  iOS7BackgroundingTutorial
+//
+//  Created by Brian H Mayo on 9/21/13.
+//  Copyright (c) 2013 CapTech Consulting. All rights reserved.
+//
+
+#import "CTCFirstViewController.h"
+
+@interface CTCFirstViewController ()
+
+@end
+
+@implementation CTCFirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOSApp/iOS7BackgroundingTutorialTests/iOS7BackgroundingTutorialTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS7BackgroundingTutorialTests.m
+//  iOS7BackgroundingTutorialTests
+//
+//  Created by Brian H Mayo on 9/21/13.
+//  Copyright (c) 2013 CapTech Consulting. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS7BackgroundingTutorialTests : XCTestCase
+
+@end
+
+@implementation iOS7BackgroundingTutorialTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
